02feb58b9dfc5d89b36fdd85f4d650289bb7b1aa
Rayco Velázquez González <vrayco@gmail.com>
2013-12-08 11:31:11 +0000

    Commit inicial, con el proyecto avanzado

---
+++ Reconocimiento de Voz - AI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace Reconocimiento_de_Voz___AI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_out = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(106, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(702, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asignatura Agentes Inteligentes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Reconocimiento_de_Voz___AI.Properties.Resources.logo_ULL_ETSII;
+            this.pictureBox1.InitialImage = global::Reconocimiento_de_Voz___AI.Properties.Resources.logo_ULL_ETSII;
+            this.pictureBox1.Location = new System.Drawing.Point(900, 12);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(423, 136);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(423, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(83, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1154, 120);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Este programa es el resultado del proyecto de la asignatura cuyo objetivo es reco" +
+    "nocer palabras del usuario y actuar en consonancia.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(861, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Alumnos: Manuel Luis Aznar - Sergio Aarón Afonso Suáres - Rayco Velázquez Gonzále" +
+    "z";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label_out
+            // 
+            this.label_out.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_out.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_out.Location = new System.Drawing.Point(17, 596);
+            this.label_out.Name = "label_out";
+            this.label_out.Size = new System.Drawing.Size(1322, 204);
+            this.label_out.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 541);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Salida del programa:";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(74, 403);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1265, 37);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Instrucciones: El programa captura las frases: \"Apagar sistema\" , \"Bajar Volúmen\"" +
+    ", \"Subir Volúmen\"";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 819);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label_out);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private NotifyIcon notifyIcon1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label_out;
+        private Label label5;
+        private Label label6;
+    }
+}
+
---
+++ Reconocimiento de Voz - AI/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace Reconocimiento_de_Voz___AI
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create a new SpeechRecognitionEngine instance.
+            sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("es-ES"));
+            
+              // Create a simple grammar that recognizes “red”, “green”, or “blue”.
+              Choices colors = new Choices();
+              colors.Add("Apagar sistema");
+              colors.Add("Bajar volumen");
+              colors.Add("Subir volumen");
+              colors.Add("Listar home");
+
+            GrammarBuilder gb = new GrammarBuilder();
+             gb.Append(colors);
+
+           // Create the actual Grammar instance, and then load it into the speech recognizer.
+           Grammar g = new Grammar(gb);
+          sre.LoadGrammar(g);
+
+      // Register a handler for the SpeechRecognized event.
+      sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+      sre.SetInputToDefaultAudioDevice();
+      sre.RecognizeAsync(RecognizeMode.Multiple);
+    }
+
+    // Simple handler for the SpeechRecognized event.
+    void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+    {
+      //label1.Text += e.Result.Text;
+      string result = ExecuteCommand("dir");
+      //MessageBox.Show(result);
+      label_out.Text += e.Result.Text+"\n";
+    }
+
+    SpeechRecognitionEngine sre;
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+        if (this.WindowState == FormWindowState.Minimized)
+        {
+            Hide();
+            notifyIcon1.Visible = true;
+        }
+    }
+
+    private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+        Show();
+        this.WindowState = FormWindowState.Normal;
+        notifyIcon1.Visible = false;
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    static string ExecuteCommand(string _Command)
+    {
+        //Indicamos que deseamos inicializar el proceso cmd.exe junto a un comando de arranque. 
+        //(/C, le indicamos al proceso cmd que deseamos que cuando termine la tarea asignada se cierre el proceso).
+        //Para mas informacion consulte la ayuda de la consola con cmd.exe /? 
+        System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + _Command);
+        // Indicamos que la salida del proceso se redireccione en un Stream
+        procStartInfo.RedirectStandardOutput = true;
+        procStartInfo.UseShellExecute = false;
+        //Indica que el proceso no despliegue una pantalla negra (El proceso se ejecuta en background)
+        procStartInfo.CreateNoWindow = false;
+        //Inicializa el proceso
+        System.Diagnostics.Process proc = new System.Diagnostics.Process();
+        proc.StartInfo = procStartInfo;
+        proc.Start();
+        //Consigue la salida de la Consola(Stream) y devuelve una cadena de texto
+        string result = proc.StandardOutput.ReadToEnd();
+        return result;
+    }
+
+    private void label2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label4_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label6_Click(object sender, EventArgs e)
+    {
+
+    }
+
+  }
+}
